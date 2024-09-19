9758e45dc981d4badf05fb97ea28a0edf2eb8edb
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-09-19 20:10:04 +0530

    Update dashboard-02 component to support dark mode

--- src/components/dashboard-02.tsx
+++ src/components/dashboard-02.tsx
@@ -3,7 +3,7 @@ import DesktopSidebar from "./desktopSidebar";
 
 const Dashboard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen dark:bg-black/95 dark:text-white w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <MobileSheet />
       <div className="flex flex-col">
         <DesktopSidebar />
